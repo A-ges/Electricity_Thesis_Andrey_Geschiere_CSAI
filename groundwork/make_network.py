cf697eccd9b49d14b90f077ad6df024cf6790e22
A-ges <andrey.geschiere@icloud.com>
2026-04-30 14:39:14 +0200

    Update make_network.py

--- groundwork/make_network.py
+++ groundwork/make_network.py
@@ -258,7 +258,8 @@ def generate_predefined_networks(agent_sizes, n_variants, output_file="networks.
     print(f"\nSaved to {output_file}")
 """
 generate_predefined_networks(
-    agent_sizes=[50, 100, 150, 200, 250, 300, 350, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 100],
+    agent_sizes=[50, 100, 150, 200, 250, 300, 350, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000],
     n_variants=5
     )
+The result can be found in networks.json, in the zipped file. Running this will again will last very long. The result is replicable
 """
